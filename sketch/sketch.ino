03cc94fbd0d1efc5d9e32b9581a3be96f840e120
Aurelien Labrosse <aurelien@skima.fr>
2023-11-16 08:11:54 +0100

    feat: Rework Arduino prototype

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -11,33 +11,31 @@
 #define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
 #endif
 
-#define ACTIVATE_MOVEMENT_INTERRUPT() sbi(PCMSK1, PCINT12)
-#define SUSPEND_MOVEMENT_INTERRUPT() cbi(PCMSK1, PCINT12)
+#define AMBIENT_LIGHT_TRESHOLD 30
+
 #define IS_MOVEMENT_DETECTED() HIGH == (digitalRead(A4))
 
-static const uint16_t SECONDS_BEFORE_AUTO_DEEP_SLEEP = 3600 * 6;
-static const uint16_t WDT_COUNT_BEFORE_DEEP_SLEEP = (SECONDS_BEFORE_AUTO_DEEP_SLEEP / 8);
 
-uint16_t wdt_it_counter;
-
+bool movementDetected = false;
 uint8_t rgbOrder = 0;
 static const uint8_t SHUTDOWN = 5;
 
 #define DEBUG
 #if defined(DEBUG)
-#define DEBUG(message)   \
+#define DEBUG(message) \
   Serial.print(message); \
   delay(20);
-#define DEBUG_LN(message)  \
+#define DEBUG_LN(message) \
   Serial.println(message); \
   delay(20);
 #else
 #define DEBUG(message)
 #define DEBUG_LN(message)
 #endif
-struct Pins
-{
-  static const uint8_t BUTTON = 2; // PD2
+struct Pins {
+  static const uint8_t BUTTON = 2;  // PD2
+  static const uint8_t BATTERY = A1;
+  static const uint8_t LUMINOSITY = A0;
   static const uint8_t GREEN = 3;
   static const uint8_t BLUE = 6;
   static const uint8_t RED = 9;
@@ -49,40 +47,37 @@ struct Pins
    Use Reduce() for actual value storing
 
 */
-struct Color
-{
+struct Color {
   // RGB intensities will be divided by this number.
   // It allows flexibility on global lamp light intensity
   static const int INTENSITY_REDUCTION_FACTOR = 10;
 
-  Color(uint8_t red, uint8_t green, uint8_t blue) : RED(Reduce(red)), GREEN(Reduce(green)), BLUE(Reduce(blue)) {}
+  Color(uint8_t red, uint8_t green, uint8_t blue)
+    : RED(Reduce(red)), GREEN(Reduce(green)), BLUE(Reduce(blue)) {}
   uint8_t RED;
   uint8_t GREEN;
   uint8_t BLUE;
 
-  static uint8_t Reduce(uint8_t in)
-  {
+  static uint8_t Reduce(uint8_t in) {
     return (in / INTENSITY_REDUCTION_FACTOR);
   }
 };
 
 static const Color rgb_setup[6] = {
-    Color(255, 0, 0) /* RED   */,
-    Color(0, 255, 0) /* GREEN */,
-    Color(0, 0, 255) /* BLUE  */,
-    Color(230, 90, 30) /* PINK  */,
-    Color(255, 88, 0) /* ORANGE */,
-    Color(0, 0, 0) /* blackout */,
+  Color(255, 0, 0) /* RED   */,
+  Color(0, 255, 0) /* GREEN */,
+  Color(0, 0, 255) /* BLUE  */,
+  Color(230, 90, 30) /* PINK  */,
+  Color(255, 88, 0) /* ORANGE */,
+  Color(0, 0, 0) /* blackout */,
 };
 
-struct InterruptBackup
-{
+struct InterruptBackup {
   uint8_t _TIMSK0;
   uint8_t _TIMSK1;
   uint8_t _TIMSK2;
 
-  void Save()
-  {
+  void Save() {
     _TIMSK0 = TIMSK0;
     _TIMSK1 = TIMSK1;
     _TIMSK2 = TIMSK2;
@@ -91,8 +86,7 @@ struct InterruptBackup
     TIMSK2 = 0;
   }
 
-  void Restore()
-  {
+  void Restore() {
     TIMSK0 = _TIMSK0;
     TIMSK1 = _TIMSK1;
     TIMSK2 = _TIMSK2;
@@ -100,16 +94,12 @@ struct InterruptBackup
 };
 InterruptBackup interruptBackup;
 
-void ApplyColor()
-{
-  if (rgbOrder < 6)
-  {
+void ApplyColor() {
+  if (rgbOrder < 6) {
     analogWrite(Pins::RED, rgb_setup[rgbOrder].RED);
     analogWrite(Pins::GREEN, rgb_setup[rgbOrder].GREEN);
     analogWrite(Pins::BLUE, rgb_setup[rgbOrder].BLUE);
-  }
-  else
-  {
+  } else {
     DEBUG("ApplyColor(): rgbOrder has inconsistent value: ");
     DEBUG_LN(rgbOrder);
   }
@@ -119,36 +109,35 @@ volatile bool buttonPressed;
 volatile bool buttonSwitched;
 volatile bool movementSensorStateHasChanged;
 
-void Sleep8s()
-{
-  DEBUG_LN("Sleep8s");
+void Sleep(period_t duration) {
+  DEBUG_LN("Sleep");
   interruptBackup.Save();
-  LowPower.idle(SLEEP_8S, ADC_OFF, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_OFF, USART0_OFF, TWI_OFF);
+  LowPower.idle(duration, ADC_OFF, TIMER2_ON, TIMER1_ON, TIMER0_ON, SPI_OFF, USART0_OFF, TWI_OFF);
   interruptBackup.Restore();
 }
 
-void DeepSleep()
-{
+void DeepSleep() {
   DEBUG_LN("DeepSleep");
-  wdt_disable();
+
+  analogWrite(Pins::RED, 0);
+  analogWrite(Pins::GREEN, 0);
+  analogWrite(Pins::BLUE, 0);
+
   interruptBackup.Save();
+
+  // movement and button can wakeup
   LowPower.powerDown(SLEEP_FOREVER, ADC_OFF, BOD_OFF);
-  interruptBackup.Restore();
 }
 
-ISR(PCINT1_vect)
-{
-  SUSPEND_MOVEMENT_INTERRUPT();
+ISR(PCINT1_vect) {
   movementSensorStateHasChanged = true;
 }
 
-void it_OnButtonChanged()
-{
+void it_OnButtonChanged() {
   buttonSwitched = true;
 }
 
-void setup()
-{
+void setup() {
   cli();
 
 #if defined(DEBUG)
@@ -159,8 +148,11 @@ void setup()
   DDRC = 0;
   DDRD = 0;
 
-  // no ADC
-  cbi(ADCSRA, ADEN);
+  pinMode(Pins::BATTERY, INPUT);
+  pinMode(Pins::LUMINOSITY, INPUT);
+
+  analogRead(Pins::BATTERY);
+  analogRead(Pins::LUMINOSITY);
 
   pinMode(Pins::RED, OUTPUT);
   pinMode(Pins::BLUE, OUTPUT);
@@ -169,118 +161,141 @@ void setup()
   // INT0 triggers on LOW level, we can
   // activate the internal pull-up
   pinMode(Pins::BUTTON, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(2), it_OnButtonChanged, LOW);
+  attachInterrupt(digitalPinToInterrupt(Pins::BUTTON), it_OnButtonChanged, CHANGE);
 
   // PC0 (A4) state change
   pinMode(A4, INPUT);
   sbi(PCICR, PCIE1);
-  ACTIVATE_MOVEMENT_INTERRUPT();
+  sbi(PCMSK1, PCINT12);
+  movementSensorStateHasChanged = false;
 
   rgbOrder = 0;
   ApplyColor();
-
-  wdt_it_counter = 0;
-  buttonPressed = false;
-  movementSensorStateHasChanged = false;
   sei();
 }
 
-void incrementRgbOrder()
-{
+void incrementRgbOrder() {
   rgbOrder++;
-  if (rgbOrder > 5)
-  {
+  if (rgbOrder > 5) {
     rgbOrder = 0;
   }
 }
 
-void loop()
-{
-  int lightValue = analogRead(A0);
-  DEBUG("Light value:");
-  DEBUG_LN(lightValue);
+struct Monitoring {
+  uint16_t luminosity;
+  uint16_t batteryVoltage;
+};
 
-  int batlevel = analogRead(A1);
-  batlevel += analogRead(A1);
-  batlevel += analogRead(A1);
-  batlevel += analogRead(A1);
-  batlevel = batlevel >> 2; // divide by 4
+static void blinkTwoTimes() {
 
-  float voltage = batlevel * (5000 / 1024);
-  DEBUG("Battery tension:");
-  DEBUG(voltage);
-  DEBUG_LN("v");
+  analogWrite(Pins::RED, 100);
+  analogWrite(Pins::GREEN, 0);
+  analogWrite(Pins::BLUE, 0);
+  _delay_ms(500);
 
-  if (movementSensorStateHasChanged == true)
-  {
-    movementSensorStateHasChanged = false;
-    ACTIVATE_MOVEMENT_INTERRUPT();
-    if (IS_MOVEMENT_DETECTED())
-    {
-      DEBUG_LN("Movement detected");
-    }
-    else
-    {
-      DEBUG_LN("End of movement detected");
-      wdt_it_counter = WDT_COUNT_BEFORE_DEEP_SLEEP;
-    }
-  }
+  analogWrite(Pins::RED, 0);
+  analogWrite(Pins::GREEN, 0);
+  analogWrite(Pins::BLUE, 0);
+  _delay_ms(500);
 
-  if (buttonSwitched)
-  {
-    buttonSwitched = false;
-    DEBUG_LN("Button switched to ");
-    if (buttonPressed)
-    {
-      buttonPressed = false;
-      DEBUG_LN("released");
-      attachInterrupt(digitalPinToInterrupt(2), it_OnButtonChanged, LOW);
-    }
-    else
-    {
-      buttonPressed = true;
-      DEBUG_LN("pressed");
+  analogWrite(Pins::RED, 100);
+  analogWrite(Pins::GREEN, 0);
+  analogWrite(Pins::BLUE, 0);
+  _delay_ms(500);
+
+  analogWrite(Pins::RED, 0);
+  analogWrite(Pins::GREEN, 0);
+  analogWrite(Pins::BLUE, 0);
+}
+
+static struct Monitoring performMonitoring() {
+  Monitoring monitoring;
+  monitoring.luminosity = analogRead(Pins::LUMINOSITY);
+  monitoring.luminosity += analogRead(Pins::LUMINOSITY);
+  monitoring.luminosity /= 2;
+  uint16_t batlevel = analogRead(Pins::BATTERY);
+  batlevel += analogRead(Pins::BATTERY);
+  batlevel += analogRead(Pins::BATTERY);
+  batlevel += analogRead(Pins::BATTERY);
+  batlevel /= 4;
+  monitoring.batteryVoltage = batlevel * (5000 / 1024);
+  return monitoring;
+}
+
+void loop() {
+
+  Monitoring monitoring = performMonitoring();
+
+#if defined(DEBUG)
+  DEBUG("LOOP: ");
+  DEBUG(monitoring.batteryVoltage);
+  DEBUG(" mV;");
+  DEBUG(monitoring.luminosity);
+  DEBUG(" ADC;");
+  DEBUG_LN(movementDetected ? "mvt" : "nomvt");
+  delay(10);
+#endif
+
+  if (buttonSwitched) {
+    bool buttonIsReleased = (digitalRead(Pins::BUTTON) == HIGH);
+    if (buttonIsReleased) {
+      detachInterrupt(digitalPinToInterrupt(Pins::BUTTON));
+      DEBUG_LN("Button clicked, color bump");
       incrementRgbOrder();
       ApplyColor();
-      if (rgbOrder == SHUTDOWN)
-      {
+      _delay_ms(500);
+      attachInterrupt(digitalPinToInterrupt(Pins::BUTTON), it_OnButtonChanged, CHANGE);
+      if (rgbOrder == SHUTDOWN) {
         rgbOrder = 0;
-        wdt_it_counter = WDT_COUNT_BEFORE_DEEP_SLEEP;
-        attachInterrupt(digitalPinToInterrupt(2), it_OnButtonChanged, LOW);
+
+#if defined(DEBUG)
+        DEBUG_LN("Manual shutdown")
+        // give time to Serial
+        delay(10);
+#endif
+        DeepSleep();
       }
-      else
-      {
-        attachInterrupt(digitalPinToInterrupt(2), it_OnButtonChanged, HIGH);
-      }
-      delay(200); // debounce
     }
   }
 
-  wdt_it_counter++;
-  if (wdt_it_counter >= WDT_COUNT_BEFORE_DEEP_SLEEP)
-  {
-    wdt_it_counter = 0;
+  if (monitoring.luminosity < AMBIENT_LIGHT_TRESHOLD) {
 
-    // movement does not wake up when deep sleeping
-    SUSPEND_MOVEMENT_INTERRUPT();
+    if (movementSensorStateHasChanged == true) {
+      DEBUG_LN("Movement event!");
+      movementSensorStateHasChanged = false;
 
-    DeepSleep();
+      if (IS_MOVEMENT_DETECTED()) {
+        DEBUG_LN("Movement is detected");
+        movementDetected = true;
 
-    detachInterrupt(digitalPinToInterrupt(2));
-    ACTIVATE_MOVEMENT_INTERRUPT();
-
-    // it_OnButtonChanged woke up MCU.
-    // we do not want to interpret this
-    // as a color change but just wakeup.
-    buttonSwitched = false;
-    buttonPressed = false;
-
+      } else {
+        DEBUG_LN("No more movement");
+        movementDetected = false;
+      }
+    }
     ApplyColor();
-    delay(500);
 
-    // wait for press again, forget release
-    attachInterrupt(digitalPinToInterrupt(2), it_OnButtonChanged, LOW);
+#if defined(DEBUG)
+    // give time to Serial
+    delay(10);
+#endif
+
+    if (movementDetected) {
+      Sleep(SLEEP_8S);
+    } else {
+      DeepSleep();
+    }
+
+  } else {
+    DEBUG_LN("Too much light!");
+#if defined(DEBUG)
+    // give time to Serial
+    delay(10);
+#endif
+    if (!buttonSwitched) {
+      blinkTwoTimes();
+    }
+    buttonSwitched = false;
+    DeepSleep();
   }
-
-  Sleep8s();
 }
